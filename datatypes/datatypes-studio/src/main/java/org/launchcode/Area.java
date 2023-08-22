4b25323c944c68f506a0ad4defe0c214a86b680c
Eric Shahan <ctibalt@gmail.com>
2023-08-21 19:45:09 -0500

    Finished Class 1 Studio Bonus Missions

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,14 +1,25 @@
 package org.launchcode;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Double radius = 0d;
+        do {
+            System.out.println("Enter a radius:");
+            try {
+                radius = input.nextDouble();
+            }
+            catch(InputMismatchException e) {
+                System.out.println("That is not a valid number!");
+                System.exit(1);
+            }
+        }
+        while (radius < 0);
 
-        System.out.println("Enter a radius:");
-        Double radius = input.nextDouble();
-//        System.out.println("The area of a circle of radius " + radius + " is: " + 3.14 * radius * radius);
+
         System.out.println(org.launchcode.Circle.getArea(radius));
     }
 }
